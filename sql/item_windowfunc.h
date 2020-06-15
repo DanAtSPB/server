ed39435c51f9fd764d08661e7cbd68242398a631
Dan Solodko <dan@mariadb.org>
2020-06-15 14:50:38 +0300

    upgrade of test, var and logic

--- sql/item_windowfunc.h
+++ sql/item_windowfunc.h
@@ -619,7 +619,7 @@ class Item_sum_ntile : public Item_sum_window_with_row_count
   Item_sum_ntile(THD* thd, Item* num_quantiles_expr) :
     Item_sum_window_with_row_count(thd, num_quantiles_expr),
     current_row_count_(0), 
-    n_old_val_(UNDEFINED) {};
+    n_first_(0) {};
 
   double val_real()
   {
@@ -634,14 +634,21 @@ class Item_sum_ntile : public Item_sum_window_with_row_count
       return 0;
     }
     longlong num_quantiles= get_num_quantiles();
-    if (n_old_val_ == UNDEFINED) 
-      n_old_val_= num_quantiles;
-    
-    if (num_quantiles <= 0 || (num_quantiles != n_old_val_)) {
+
+    if (num_quantiles <= 0) 
+    {
       my_error(ER_INVALID_NTILE_ARGUMENT, MYF(0));
       return true;
     }
+    else if (n_first_ == 0) 
+           n_first_= num_quantiles;
+         else if (num_quantiles != n_first_) 
+         {
+           my_error(ER_INVALID_NTILE_ARGUMENT, MYF(0));
+           return true;
+         }
     null_value= false;
+
     ulonglong quantile_size = get_row_count() / num_quantiles;
     ulonglong extra_rows = get_row_count() - quantile_size * num_quantiles;
 
@@ -666,7 +673,7 @@ class Item_sum_ntile : public Item_sum_window_with_row_count
   {
     current_row_count_= 0;
     set_row_count(0);
-    n_old_val_= UNDEFINED;
+    n_first_= 0;
   }
 
   const char*func_name() const
@@ -685,8 +692,7 @@ class Item_sum_ntile : public Item_sum_window_with_row_count
  private:
   longlong get_num_quantiles() { return args[0]->val_int(); }
   ulong current_row_count_;
-  longlong n_old_val_; 
-  enum type { UNDEFINED };
+  longlong n_first_;
 };
 
 
